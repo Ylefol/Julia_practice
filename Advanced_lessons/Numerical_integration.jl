4f80fe053c22564aae3cc981090de4b8e3812643
YLefol <yohan.lefol@gmail.com>
2020-08-27 08:30:12 +0200

    finished integral lesson

--- Advanced_lessons/Numerical_integration.jl
+++ Advanced_lessons/Numerical_integration.jl
@@ -16,4 +16,26 @@ println(abs(res-sqrt(pi))/sqrt(pi))
 #The default order is seven and often enough to deal with any issues
 
 ##Functions with multiple arguments
-println("blarg")
+
+func2(x,y,z) = x + y^3 +sin(z)
+
+x=5
+z=3
+arg(y) = func2(x,y,z)
+
+res,err = let x=5; z=3
+    arg(y) = func2(x,y,z)
+
+    quadgk(arg,1,3)
+end
+
+func3(x,y)=x^2*exp(y)
+
+function test_int(x, ymin, ymax)
+    arg(y) = func3(x,y)
+    return quadgk(arg, ymin, ymax)[1]
+end
+
+println(test_int(3,1,5))
+
+## Mostly math stuff that I won't use
